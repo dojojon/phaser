3ccc47684c33eaaa92600a09f7cafb3faf99ca4b
DojoJon <dojo@outlook.ie>
2018-03-10 18:05:50 +0000

    Move map to its own class

---
+++ dungeon/objects/map.js
@@ -0,0 +1,65 @@
+import { ChestGroup } from './chest-group.js';
+import { DoorGroup } from './door-group.js';
+import { MAPSCALE } from '../settings.js';
+import { MapGroup } from './map-group.js';
+
+export class Map {
+
+    constructor(game) {
+
+        this.game = game;
+
+        this.map = this.game.add.tilemap('test_map');
+        this.map.addTilesetImage('basic', 'basic_tiles');
+
+        this.mineLayer = this.map.createLayer('Mine');
+        this.mineLayer.setScale(MAPSCALE);
+        this.mineLayer.resizeWorld();
+
+        this.mineLayer.debug = false;
+
+        this.map.setCollisionBetween(0, 7, true, this.mineLayer);
+
+        // Create objects from the object layers
+        this.coinsGroup = this.createObjectGroup();
+        this.coinsGroup.fillGroup(69, 'tiles', 68);
+
+        this.gemGroup = this.createObjectGroup();
+        this.coinsGroup.fillGroup(70, 'tiles', 69);
+
+        this.blockingObjects = this.createObjectGroup();
+        this.blockingObjects.fillGroup(59, 'tiles', 58); // rocks
+        this.blockingObjects.fillGroup(55, 'tiles', 54); // table
+        this.blockingObjects.fillGroup(46, 'tiles', 45); // bed head
+        this.blockingObjects.fillGroup(54, 'tiles', 53); // bed foot
+        this.blockingObjects.fillGroup(40, 'tiles', 39); // column
+        this.blockingObjects.fillGroup(32, 'tiles', 31); // well
+        this.blockingObjects.fillGroup(48, 'tiles', 47); // statue
+        this.blockingObjects.fillGroup(47, 'tiles', 46); // chair
+
+        // Doors have an additional animation
+        this.doorsGroup = new DoorGroup(this.game, this.map);
+
+        // Chests have an additional animation
+        this.chestsGroup = new ChestGroup(this.game, this.map);;
+
+    }
+
+    createObjectGroup() {
+
+        return new MapGroup(this.game, this.map);
+
+    }
+
+    update(player) {
+
+        this.game.physics.arcade.collide(player, this.mineLayer);
+
+        this.game.physics.arcade.collide(player, this.coinsGroup, this.coinsGroup.collide);
+        this.game.physics.arcade.collide(player, this.gemGroup, this.gemGroup.collide);
+        this.game.physics.arcade.collide(player, this.blockingObjects, this.blockingObjects.collide);
+        this.game.physics.arcade.collide(player, this.doorsGroup, this.doorsGroup.collide);
+        this.game.physics.arcade.collide(player, this.chestsGroup, this.chestsGroup.collide);
+
+    }
+}
